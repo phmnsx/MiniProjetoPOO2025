2671afc66475080a1b75abb557852d3361b450ec
KevinTeixeira04 <139189795+KevinTeixeira04@users.noreply.github.com>
2025-07-19 13:51:14 -0300

    colocação da classe itemnota

--- src/main/java/Nota/ItemNota.java
+++ src/main/java/Nota/ItemNota.java
@@ -1,1 +1,41 @@
+package Nota;
+import java.math.BigDecimal;
+import Produto.Produto;
+
+public class ItemNota {
+	private Produto produto;
+	private int quantidade;
+	
+	public ItemNota (Produto produto, int quantidade){
+		this.produto = produto;
+		this.quantidade = quantidade;
+		}
+
+	 public Produto getProduto() {
+        return produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public BigDecimal getPrecoUnitario() {
+        return produto.getPreco(); 
+    }
+
+
+    public BigDecimal getPrecoTotal() {
+        return produto.getPreco().multiply(BigDecimal.valueOf(quantidade));
+    }
+	
+	public void printItemNota(){
+		
+	  System.out.println("Produto: " + produto.getNome());
+	  System.out.println("Codigo: " + produto.getCodigo());
+	  System.out.println("Quantidade: " + quantidade);
+	  System.out.println("Preco unitario: " + getPrecoUnitario());
+	  System.out.println("Total: " + getPrecoTotal());
+	  System.out.print ("\n\n");
+		}
+}
 
