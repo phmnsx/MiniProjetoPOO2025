1eaa7dca28ecf6f30d566be8083d547f08e8ed42
phmnsx <lynnwastinghertime@gmail.com>
2025-07-19 14:30:20 -0300

    ajustes a nota e itemnota

--- src/main/java/Nota/ItemNota.java
+++ src/main/java/Nota/ItemNota.java
@@ -3,39 +3,27 @@ import java.math.BigDecimal;
 import Produto.Produto;
 
 public class ItemNota {
-	private Produto produto;
-	private int quantidade;
-	
-	public ItemNota (Produto produto, int quantidade){
-		this.produto = produto;
-		this.quantidade = quantidade;
-		}
+    private Produto produto;
+    private int quantidade;
+    private BigDecimal subtotal;
 
-	 public Produto getProduto() {
-        return produto;
+    public ItemNota (Produto produto, int quantidade){
+        this.produto = produto;
+        this.quantidade = quantidade;
+        this.subtotal = produto.getPreco().multiply(BigDecimal.valueOf(quantidade));
     }
-
-    public int getQuantidade() {
-        return quantidade;
-    }
-
-    public BigDecimal getPrecoUnitario() {
-        return produto.getPreco(); 
-    }
-
-
     public BigDecimal getPrecoTotal() {
-        return produto.getPreco().multiply(BigDecimal.valueOf(quantidade));
+        return subtotal;
     }
-	
-	public void printItemNota(){
-		
-	  System.out.println("Produto: " + produto.getNome());
-	  System.out.println("Codigo: " + produto.getCodigo());
-	  System.out.println("Quantidade: " + quantidade);
-	  System.out.println("Preco unitario: " + getPrecoUnitario());
-	  System.out.println("Total: " + getPrecoTotal());
-	  System.out.print ("\n\n");
-		}
+
+    public void printItemNota(){
+
+        System.out.println("Produto: " + produto.getNome());
+        System.out.println("Codigo: " + produto.getCodigo());
+        System.out.println("Quantidade: " + quantidade);
+        System.out.println("Preco unitario: " + getPrecoUnitario());
+        System.out.println("Total: " + this.subtotal);
+        System.out.print ("\n");
+    }
 }
 
--- src/main/java/Nota/Nota.java
+++ src/main/java/Nota/Nota.java
@@ -1,79 +1,61 @@
 package Nota;
 import java.time.LocalDate;
 import java.math.BigDecimal;
-import Nota.ItemNota;
 import Produto.Produto;
 import Cliente.Cliente;
 
 public class Nota {
-	private Cliente pessoa;
-	private BigDecimal subtotal;
-	private ItemNota[] listaProdutos;
-	private String id;
-	private LocalDate data;
-	private static int ultimoId = 0;
+private Cliente pessoa;
+private BigDecimal subtotal;
+private ItemNota[] listaProdutos;
+private String id;
+private LocalDate data;
+private static int ultimoId = 0;
 
 	
-	public Nota (Cliente pessoa){
-		this.pessoa = pessoa;
-		this.subtotal = new BigDecimal(0f);
-		this.id = Integer.toString(ultimoId++); 
-		this.data = LocalDate.now();
-		}
-	
-	public void colocaProduto (Produto produto, int quantidade){
-		ItemNota item = new ItemNota(produto, quantidade);
-		ItemNota [] listaItemNota = new ItemNota[listaProdutos.length + 1];
-		System.arraycopy(listaProdutos, 0, listaItemNota, 0, listaProdutos.length);
-		listaItemNota[listaProdutos.length] = item;
-		listaProdutos = listaItemNota;
-		
-		subtotal = subtotal.add(item.getPrecoTotal());
-		printNota();
-		}
-		
-	public Cliente getPessoa(){
-		return this.pessoa;
-		}
-	
-	public BigDecimal getSubtotal(){
-		return this.subtotal;
-		}
-	
+public Nota (Cliente pessoa){
+    this.pessoa = pessoa;
+    this.subtotal = new BigDecimal(0f);
+    this.id = Integer.toString(ultimoId++); 
+    this.data = LocalDate.now();
+    }
+
+    public void colocaProduto (Produto produto, int quantidade){
+        ItemNota item = new ItemNota(produto, quantidade);
+        ItemNota [] listaItemNota = new ItemNota[listaProdutos.length + 1];
+        System.arraycopy(listaProdutos, 0, listaItemNota, 0, listaProdutos.length);
+        listaItemNota[listaProdutos.length] = item;
+        listaProdutos = listaItemNota;
+
+        subtotal = subtotal.add(item.getPrecoTotal());
+    }
+
+    public Cliente getPessoa(){
+        return this.pessoa;
+    }
+
+    public BigDecimal getSubtotal(){
+        return this.subtotal;
+    }
+
     public String getId(){
         return this.id;
     }
-    
-    public LocalDate getData() {
-    return this.data;
-	}
 
-	public void setPessoa (Cliente pessoa){
-		this.pessoa = pessoa;
-		}
-		
-	public void setSubtotal (BigDecimal subtotal){
-		this.subtotal = subtotal;
-		}
-	
-	public void setId (String id){
-		this.id = id;
-		}
-		
-	public void setData(LocalDate novaData) {
-    this.data = novaData;
-	}
-	
-	public void printNota (){
-		System.out.println ("Cliente: " + this.pessoa);
-		System.out.println ("Subtotal: " + this.subtotal);
-		System.out.println ("ID: " + this.id);
-		System.out.println("Data da compra: " + this.data);
-		System.out.print ("\n\n");
-		System.out.println("Itens da nota:");
-		for (int i = 0; i < listaProdutos.length; i++) {
-        listaProdutos[i].printItemNota();
-		}
-		}
-		
+    public LocalDate getData() {
+        return this.data;
+    }
+
+    public void emitirNota (){
+        System.out.println ("Cliente: " + this.pessoa);
+        System.out.println ("Subtotal: " + this.subtotal);
+        System.out.println ("ID: " + this.id);
+        System.out.println("Data da compra: " + this.data);
+        System.out.println("Itens da compra:");
+
+        for (int i = 0; i < listaProdutos.length; i++) {
+            listaProdutos[i].printItemNota();
+        }
+    }
+
 }
